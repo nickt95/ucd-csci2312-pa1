1114ab2cdbbeb1cb1c5afc02e4fa80a784c1e7b5
nickt95 <nicholas.thomason@ucdenver.edu>
2015-09-05 14:15:45 -0600

    PA1

--- Point.cpp
+++ Point.cpp
@@ -1,17 +1,20 @@
 #include "Point.h"
+#include <cmath>
 
 // Default constructor
-// Initializes the point to (0.0, 0.0)
+// Initializes the point to (0.0, 0.0, 0.0)
 Point::Point() {
   x = 0.0;
   y = 0.0;
+  z = 0.0;
 }
 
 // Constructor
-// Initializes the point to (initX, initY)
-Point::Point(double initX, double initY) {
+// Initializes the point to (initX, initY, intiZ)
+Point::Point(double initX, double initY, double intiZ) {
   x = initX;
   y = initY;
+  z = intiZ;
 }
 
 // Destructor
@@ -31,6 +34,10 @@ void Point::setY(double newY) {
   y = newY;
 }
 
+void Point::setZ(double newZ) {
+  z = newZ;
+}
+
 // Accessors
 // Return the current values of private member variables
 
@@ -40,4 +47,12 @@ double Point::getX() {
 
 double Point::getY() {
   return y;
+}
+
+double Point::getZ() {
+  return z;
+}
+
+double Point::distanceTo(Point &other) {
+  return sqrt(pow(this->getX()-other.getX(),2)+pow(this->getY()-other.getY(),2)+pow(this->getZ()-other.getZ(),2));
 }
--- pa1.cpp
+++ pa1.cpp
@@ -1,11 +1,64 @@
 //
-// Created by Ivo Georgiev on 8/25/15.
+// Created by Nick Thomason on 9/4/15.
 //
 
 #include "Point.h"
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+double computeArea(Point &a,Point &b,Point &c){
+    //Find side lengths
+    double sideA = b.distanceTo(c);
+    double sideB = c.distanceTo(a);
+    double sideC = a.distanceTo(b);
+
+    //Find semiperimeter
+    double semiPer = (sideA+sideB+sideC)/2.0;
+
+    //Return area as found by Heron's formula
+    return sqrt(semiPer*(semiPer-sideA)*(semiPer-sideB)*(semiPer-sideC));
+}
 
 int main(void) {
-    Point p;
+    //set precision of double
+    cout << fixed << setprecision(2);
 
+    //initialize temporary variables
+    double tempX, tempY, tempZ;
+
+    //ask user for the first point of the triangle
+    cout << "What is the X-position of point A?" << endl;
+    cin >> tempX;
+    cout << "What is the Y-position of point A?" << endl;
+    cin >> tempY;
+    cout << "What is the Z-position of point A?" << endl;
+    cin >> tempZ;
+    Point a(tempX, tempY, tempZ);
+
+    //ask user for the second point of the triangle
+    cout << "What is the X-position of point B?" << endl;
+    cin >> tempX;
+    cout << "What is the Y-position of point B?" << endl;
+    cin >> tempY;
+    cout << "What is the Z-position of point B?" << endl;
+    cin >> tempZ;
+    Point b(tempX, tempY, tempZ);
+
+    //ask user for the third point of the triangle
+    cout << "What is the X-position of point C?" << endl;
+    cin >> tempX;
+    cout << "What is the Y-position of point C?" << endl;
+    cin >> tempY;
+    cout << "What is the Z-position of point C?" << endl;
+    cin >> tempZ;
+    Point c(tempX, tempY, tempZ);
+
+    //print the area of the triangle
+    cout << "The area of the triangle is " << computeArea(a,b,c) << "unit(s)." << endl;
+
+    //end program
     return 0;
 }
